e6a8280cc07570aa55b610fe790aac83cb44085d
AbdiAbader <abdiprocode@gmail.com>
2022-09-07 16:14:09 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,67 @@
+#include "monty.h"
+data_t datas = {NULL ,NULL};
+/**
+ *main - main function for monty interpreter
+ *@argc: argumnets  conuter
+ *@argv: arguments char
+ *Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+void (*temp)(stack_t **, unsigned int);
+int line_number = 0;
+char buf[BUFSIZ], *token;
+if (argc != 2)
+{
+fprintf(stderr, "USAGE: monty file\n");
+exit(EXIT_FAILURE);
+}
+datas.file = fopen(argv[1], "r");
+if (datas.file == NULL)
+{
+fprintf(stderr, "Error: Can't open file %s\n",argv[1]);
+exit(EXIT_FAILURE);
+}
+while (fgets(buf,sizeof buf,datas.file) != NULL)
+{
+line_number++;
+token = strtok(buf," \t\n\r");
+if(!(temp =opr(token)))
+{
+fprintf(stderr, "L%d: unknown instruction %s\n", line_number, token);
+free_memo(datas.list);
+fclose(datas.file);
+exit(EXIT_FAILURE);
+}
+temp(&datas.list, line_number);
+}
+free_memo(datas.list);
+fclose(datas.file);
+return (0);
+}
+/**
+*opr - choose functions if exist
+*@f1: function name
+*@head: head of the list
+*@line_number: number of line
+*Return: Nothing
+*/
+void (*opr(char *f1))(stack_t **head, unsigned int line_number)
+{
+instruction_t ops[] = {
+{"push", push},
+{"pall", pall},
+{NULL, NULL}
+};
+int i = 0;
+while (ops[i].opcode)
+{
+
+if (strcmp(f1, ops[i].opcode) == 0)
+{
+break;
+}
+i++;
+}
+return (ops[i].f);
+}
---
+++ stack_1.c
@@ -0,0 +1,80 @@
+#include "monty.h"
+/**
+ *push - pushs value to the stack
+ *@value: number of line
+ *Return: nothing
+ */
+void push(stack_t **head, __attribute__ ((unused)) unsigned int value)
+{
+  int i;
+char *token;
+token = strtok(NULL, " \t\n\r");
+if (!token)
+{
+fprintf(stderr, "L%d: usage: push integer\n", value);
+free_memo(datas.list);
+fclose(datas.file);
+exit(EXIT_FAILURE);
+}
+ for (i = 0;token[i];i++)
+   {
+     if (token[i] >= 65 && token[i] <=122)
+       {
+fprintf(stderr, "L%d: usage: push integer\n", value);
+free_memo(datas.list);
+fclose(datas.file);
+ exit(EXIT_FAILURE);}
+}
+ add(head,atoi(token));
+}
+/**
+ *add - adds to doubly linked list
+ *@head: head of the list
+ *@value: number of lines
+ *return: new address for head
+ */
+stack_t *add(stack_t **head, __attribute__ ((unused)) int value)
+{
+stack_t *new = malloc(sizeof(stack_t));
+if (head == NULL)
+return (NULL);
+if (new == NULL)
+{
+fprintf(stderr, "Error: malloc failed\n");
+free_memo(datas.list);
+fclose(datas.file);
+exit(EXIT_FAILURE);
+}
+new->n = value;
+new->next = *head;
+new->prev = NULL;
+*head = new;
+if (new->next != NULL)
+(new->next)->prev = new;
+return (new);
+}
+void pall(stack_t **head, __attribute__ ((unused)) unsigned int line_number)
+{
+stack_t *current = NULL;
+current = *head;
+while (current != NULL)
+{
+printf("%d\n", current->n);
+current = current->next;
+}
+}
+/**
+*free - free memory
+*@head: head of the list
+*Return: void
+*/
+void free_memo(stack_t *head)
+{
+stack_t *hold;
+while (head)
+{
+hold = head;
+head = head->next;
+free(hold);
+}
+}
