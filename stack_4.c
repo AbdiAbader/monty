5286771969a5797ee6e7fe72c8bf4f4545241bf8
AbdiAbader <abdiprocode@gmail.com>
2022-09-08 07:26:38 +0300

    rotl updated

--- stack_4.c
+++ stack_4.c
@@ -1,47 +1,20 @@
 #include "monty.h"
 /**
-*add_end - add node at the end
+*rotls - rotates top of the elemenets
 *@head: head of the list
-*@n: number
-*Return: address of the head
+*@value: line number
+*Return: none
 */
-stack_t *add_end(stack_t **head, const int n)
-{
-stack_t *new, *last;
-new = malloc(sizeof(stack_t));
-if (new == NULL)
-{
-fprintf(stderr, "Error: malloc failed\n");
-free_memo(datas.list);
-fclose(datas.file);
-exit(EXIT_FAILURE);
-}
-new->n = n;
-new->next = NULL;
-
-if (*head == NULL)
-{
-new->prev = NULL;
-*head = new;
-return (new);
-}
-
-last = *head;
-while (last->next != NULL)
-last = last->next;
-last->next = new;
-new->prev = last;
-
-return (new);
-}
-/**
- *rotls - rotates top of the elemenets
- *@head: head of the list
- *@value: line number
- *Return: none
- */
 void rotls(stack_t **head, __attribute__ ((unused)) unsigned int value)
 {
-add_end(head, (*head)->n);
-pop(head, value);
+int temp;
+stack_t *first = *head, *last = *head;
+temp = first->n;
+while (last->next != NULL)
+{
+if (last->next != NULL)
+last->n = last->next->n;
+last = last->next;
+}
+last->n = temp;
 }
